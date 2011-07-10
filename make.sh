2297deded7da66513893d195782440354d2347f9
Amitaibu <amitai-git@gizra.com>
2011-07-10 20:26:16 +0300

    Remove old make file.

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Prepare an installation.
-
-sudo rm -rf /Applications/MAMP/htdocs/plm && drush make /Applications/MAMP/htdocs/opengizra/stub.make /Applications/MAMP/htdocs/plm  --prepare-install --working-copy && chmod 777 -R /var/www/test/sites/default/ && cp /var/www/test/sites/default/default.settings.php /var/www/test/sites/default/settings.php && cd /var/www/test && drush si --db-url=mysql://root@localhost/opengizra --profile opengizra -y
-
-# cd /var/www/test && drush si --db-url=mysql://root@localhost/opengizra --profile opengizra -y
