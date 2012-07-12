fd82f4372497fa38a00e73370afe66bfdd5538dd
Amitaibu <amitai@gizra.com>
2012-07-12 11:42:14 +0300

    Add uri to bash.

--- opengizra.sh
+++ opengizra.sh
@@ -1,9 +1,10 @@
 NEWDIR=/Applications/MAMP/htdocs/opengizra_build
 PROFILE=opengizra
 DB=opengizra_build
+URI=http://local:8888/opengizra_build
 
 bash scripts/build.sh -y $NEWDIR
 
 cd $NEWDIR
 drush sql-drop -y
-drush si -y $PROFILE --account-pass=admin --db-url=mysql://root:root@localhost/$DB
+drush si -y $PROFILE --account-pass=admin --db-url=mysql://root:root@localhost/$DB --uri=$URI
