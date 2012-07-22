6e583aeee8c4d4208539ee0f4172c30e3525ed8f
Amitaibu <amitai@gizra.com>
2012-07-22 15:10:04 +0300

    More line sheet fixes.

--- opengizra/modules/opengizra_line_sheet/plugins/content_types/line_sheet/line-sheet.tpl.php
+++ opengizra/modules/opengizra_line_sheet/plugins/content_types/line_sheet/line-sheet.tpl.php
@@ -1,7 +1,18 @@
 <div class="line-sheet-item">
-  <?php print $body; ?>
-  <?php print $sizes; ?>
-  <?php print $images; ?>
+  <?php if ($body): ?>
+    <div class="description"><?php print $body; ?></div>
+  <?php endif; ?>
+
+  <?php if ($sizes): ?>
+    <label><?php print t('Sizes'); ?></label>
+    <?php print $sizes; ?>
+  <?php endif; ?>
+
+  <?php if ($images): ?>
+    <div class="images"><?php print $images; ?></div>
+  <?php endif; ?>
+
+  <?php print $links; ?>
 </div>
 
 
