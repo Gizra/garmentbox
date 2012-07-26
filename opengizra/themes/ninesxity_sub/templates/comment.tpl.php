7f6a541434e5b0646cbb60963e1952c5b40d10d0
Amitaibu <amitai@gizra.com>
2012-07-26 16:16:21 +0300

    Create a dummy comment to hold the task's initial state.

--- opengizra/themes/ninesxity_sub/templates/comment.tpl.php
+++ opengizra/themes/ninesxity_sub/templates/comment.tpl.php
@@ -1,18 +1,20 @@
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<?php if ($show_comment): ?>
+  <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print $permalink; ?>
-  <?php print $picture; ?>
-  <?php if ($changed_fields): ?>
-    <div class="changed-properties"><?php print $changed_fields; ?></div>
-  <?php endif; ?>
-  <div class="content"<?php print $content_attributes; ?>>
-    <div class="submitted">
-      <?php print $author; ?>
+    <?php print $permalink; ?>
+    <?php print $picture; ?>
+    <?php if ($changed_fields): ?>
+      <div class="changed-properties"><?php print $changed_fields; ?></div>
+    <?php endif; ?>
+    <div class="content"<?php print $content_attributes; ?>>
+      <div class="submitted">
+        <?php print $author; ?>
+      </div>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['links']);
+        print render($content);
+      ?>
     </div>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['links']);
-      print render($content);
-    ?>
   </div>
-</div>
+<?php endif; ?>
