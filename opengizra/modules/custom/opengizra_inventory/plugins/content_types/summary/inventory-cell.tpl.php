97eb0621905365e18ce6c1402f2d0a78bc4dc1b0
Itamar Bar-Lev <itamar@gizra.com>
2012-10-25 17:17:54 +0200

    refs #9: Improving inventory view.

--- opengizra/modules/custom/opengizra_inventory/plugins/content_types/summary/inventory-cell.tpl.php
+++ opengizra/modules/custom/opengizra_inventory/plugins/content_types/summary/inventory-cell.tpl.php
@@ -1,6 +1,15 @@
-<div class="quantity"><?php print $quantity; ?></div>
-<?php if ($availability): ?>
-  <div class="availability <?php print $class; ?>">
-    <span class="amount"><?php print $availability; ?></span><span class="label"><?php print $label; ?></span>
+<div class="inventory-cell clearfix">
+  <div class="main-figure">
+    <span class="amount <?php if ($quantity_title): ?>with-title<?php endif; ?>"><?php print $quantity; ?></span><?php if ($quantity_title): ?><span class="label"><?php print $quantity_title; ?></span><?php endif; ?>
   </div>
-<?php endif; ?>
+
+  <div class="sub-figures">
+    <?php foreach($subfigure_types as $type => $label): ?>
+      <?php if ($$type || $type == 'available' && $$type !== NULL): ?>
+        <div class="sub-figure <?php print $type; ?>">
+          <span class="amount"><?php print $$type; ?></span><span class="label"><?php print $label; ?></span>
+        </div>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </div>
+</div>
