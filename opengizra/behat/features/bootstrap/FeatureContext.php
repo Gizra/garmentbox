7ddb4c9be0ca082ab8fb7179236cd71e54e65980
Itamar Bar-Lev <itamar@gizra.com>
2012-10-31 11:37:33 +0200

    Chmoding public files.

--- opengizra/behat/features/bootstrap/FeatureContext.php
+++ opengizra/behat/features/bootstrap/FeatureContext.php
@@ -255,19 +255,9 @@ class FeatureContext extends DrupalContext {
       throw new \Exception('Missing image tag.');
     }
 
-    ////////////// Temporary travis test:
-    $client = new Client();
-
-    $response = $client->get('http://127.0.0.1/user')->send();
-    $info = $response->getInfo();
-
-    $response = $client->get($image_url . 'realynotthere.jpg')->send();
-    $info = $response->getInfo();
-    ////////////
-
     // Send a GET request to the image to make sure it's accessible.
     $image_url = $image_element->getAttribute('src');
-
+    $client = new Client();
     $response = $client->get($image_url)->send();
     $info = $response->getInfo();
     if ($info['http_code'] != 200) {
