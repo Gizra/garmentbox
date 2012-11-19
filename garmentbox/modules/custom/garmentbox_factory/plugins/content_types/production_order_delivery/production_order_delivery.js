194b09fe68061c841d84400db5694c11ec800c68
Itamar Bar-Lev <itamar@gizra.com>
2012-11-19 10:04:26 +0200

    Adding "total items"

--- garmentbox/modules/custom/garmentbox_factory/plugins/content_types/production_order_delivery/production_order_delivery.js
+++ garmentbox/modules/custom/garmentbox_factory/plugins/content_types/production_order_delivery/production_order_delivery.js
@@ -57,6 +57,7 @@ Drupal.behaviors.GarmentboxOrderItems = {
       }
       this.updateVariantPrice(context, nid);
     }
+    this.updateTotals(context);
   },
 
   // Update the production price of a specific variant.
@@ -75,14 +76,31 @@ Drupal.behaviors.GarmentboxOrderItems = {
       // Sum the row quantity.
       var items_count = 0;
       row.find(query).each(function(i, element) {
-        var quantity = $(element).is('input') ? parseInt($(element).val()) : $(element).text();
+        var quantity = $(element).is('input') ? $(element).val() : $(element).text();
+        quantity = parseInt(quantity);
         if (!isNaN(quantity) && quantity >= 0) {
           items_count += quantity;
         }
       });
       var price = items_count * item_price;
       row.find('td.price').text('$' + Drupal.formatNumber(price, 2));
+      // Save the variant's items count for the totals calculation.
+      Drupal.settings.garmentbox_factory.delivery_data[nid].items_count[types[i]] = items_count;
     }
+  },
+
+  // Update the "Total items" and "Production cost" elements.
+  updateTotals: function(context) {
+    var receivedItems = 0;
+    var receivedItemsPrice = 0;
+    for (nid in Drupal.settings.garmentbox_factory.delivery_data) {
+      var variantReceivedItems = Drupal.settings.garmentbox_factory.delivery_data[nid].items_count.received;
+      receivedItems += variantReceivedItems;
+      receivedItemsPrice += variantReceivedItems * (Drupal.settings.garmentbox_factory.delivery_data[nid].item_price / 100);
+    }
+
+    $(context).find('input#edit-total-items-new').val(receivedItems);
+    $(context).find('input#edit-production-cost-new').val('$' + Drupal.formatNumber(receivedItemsPrice, 2));
   }
 };
 
