687973b4d786f42e85ac3685c5f7a8c5088295b8
Amitai Burstein <amitai@gizra.com>
2014-02-12 23:21:49 +0200

    fix alias.

--- garmentbox/behat/aliases.drushrc.php
+++ garmentbox/behat/aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['garmentbox'] = array(
   'uri' => 'http://127.0.0.1/',
-  'root' => '/home/travis/builds/Gizra/Garment-Box/www',
+  'root' => '/home/travis/builds/Gizra/garmentbox/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
