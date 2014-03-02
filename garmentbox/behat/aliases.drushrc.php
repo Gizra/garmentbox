67c6b6be9f1be266e904d8c7637f9683d6203b24
Amitaibu <amitai@gizra.com>
2014-03-02 09:50:12 +0200

    Set port.

--- garmentbox/behat/aliases.drushrc.php
+++ garmentbox/behat/aliases.drushrc.php
@@ -1,7 +1,7 @@
 <?php
 
 $aliases['garmentbox'] = array(
-  'uri' => 'http://127.0.0.1/',
+  'uri' => 'http://127.0.0.1:8080',
   'root' => '/home/travis/build/Gizra/garmentbox/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
