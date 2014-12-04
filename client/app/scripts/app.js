465cc55bd0c26975935c842f87ba345fe9de2fd9
Amitai Burstein <amitai@gizra.com>
2014-12-04 22:19:37 +0200

    More work

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -14,24 +14,59 @@ angular
     'ngCookies',
     'ngSanitize',
 
+    'config',
+    'LocalStorageModule',
     'ui.router'
   ])
-  .config(function($stateProvider, $urlRouterProvider) {
-    //
-    // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/state1");
-    //
-    // Now set up the states
+  .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    // Now set up the states.
     $stateProvider
-      .state('items', {
-        url: "/items",
-        templateUrl: "views/state1.html"
+      .state('dashboard', {
+        url: '/',
+        templateUrl: 'views/dashboard.html',
+        controller: 'MainCtrl'
       })
-      .state('items.item', {
-        url: "/:id",
-        templateUrl: "partials/state1.list.html",
-        controller: function($scope) {
-          $scope.items = ["A", "List", "Of", "Items"];
-        }
+      .state('dashboard.login', {
+        url: 'login',
+        templateUrl: 'views/dashboard.login.html',
+        controller: 'MainCtrl'
+      })
+      .state('dashboard.main', {
+        url: 'dashboard',
+        templateUrl: 'views/dashboard.main.html',
+        controller: 'MainCtrl'
       });
+
+    // For any unmatched url, redirect to '/'.
+    $urlRouterProvider.otherwise('/');
+
+    // Define interceptors.
+    $httpProvider.interceptors.push(function ($q, Auth, localStorageService) {
+      return {
+        'request': function (config) {
+          if (!config.url.match(/login-token/)) {
+            config.headers = {
+              'access_token': localStorageService.get('access_token')
+            };
+          }
+          return config;
+        },
+
+        'response': function(result) {
+          if (result.data.access_token) {
+            localStorageService.set('access_token', result.data.access_token);
+          }
+          return result;
+        },
+
+        'responseError': function (response) {
+          if (response.status === 401) {
+            Auth.authFailed();
+          }
+
+          return $q.reject(response);
+        }
+      };
+    });
   });
