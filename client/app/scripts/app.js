ec88bb110d9f02e0b1f6838e663157a3e22b97ac
Amitai Burstein <amitai@gizra.com>
2014-12-07 17:45:07 +0200

    Add debug

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -69,14 +69,13 @@ angular
         onEnter: redirectToDashbaord
       })
       .state('dashboard', {
-        url: '',
+        url: '/dashboard',
         templateUrl: 'views/dashboard/main.html',
         controller: 'DashboardCtrl',
-        abstract: true,
         onEnter: page403
       })
       .state('dashboard.items', {
-        url: '',
+        url: '/items',
         templateUrl: 'views/dashboard/items/items.html',
         controller: 'ItemsCtrl',
         onEnter: page403
@@ -141,4 +140,13 @@ angular
         }
       };
     });
+  })
+  .run(function ($rootScope,   $state,   $stateParams) {
+
+    // It's very handy to add references to $state and $stateParams to the $rootScope
+    // so that you can access them from any scope within your applications.For example,
+    // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+    // to active whenever 'contacts.list' or one of its decendents is active.
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
   });
